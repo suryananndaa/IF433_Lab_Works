4b4fcd7dec90b538a0f170f40e8e446be5bad59d
Surya Nanda <suryananda7963@gmail.com>
2026-02-02 23:36:37 +0700

    week01: (tugas 2) setup GameStore file

--- src/oop_001_suryanandadestiawan/week01/GameStore.kt
+++ src/oop_001_suryanandadestiawan/week01/GameStore.kt
@@ -4,7 +4,3 @@ fun main() {
     val gameTitle = "Cyber Adventure 2077"
     val price = 650000
 }
-
-fun calculateDiscount(price: Int) =
-    if (price > 500000) price * 80 / 100
-    else price * 90 / 100
