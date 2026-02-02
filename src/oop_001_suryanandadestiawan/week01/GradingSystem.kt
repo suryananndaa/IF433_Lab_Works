58d4a8c72024cc336c34d4075b028738b65fb2cf
Surya Nanda <suryananda7963@gmail.com>
2026-02-02 22:47:00 +0700

    week01: initial setup and procedural code

---
+++ src/oop_001_suryanandadestiawan/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_001_suryanandadestiawan.week01
+
+fun main() {
+    var name: String = "Surya Nanda"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
