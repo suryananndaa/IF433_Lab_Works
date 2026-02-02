aac5dfcefacf7cd3b8c2db20e8dd59082ca82958
Surya Nanda <suryananda7963@gmail.com>
2026-02-02 22:50:19 +0700

    week01: implement grading logic using when expression

--- src/oop_001_suryanandadestiawan/week01/GradingSystem.kt
+++ src/oop_001_suryanandadestiawan/week01/GradingSystem.kt
@@ -7,4 +7,12 @@ fun main() {
 
     // refactor: string template ($name)
     println("Nama: $name, Nilai: $score")
+
+    val grade = when (score) {
+        in 90..100 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+    println("Grade kamu: $grade")
 }
