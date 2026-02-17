17cce19583e53e12511bb725c20c4a1906c5eff6
suryananndaa <suryananda7963@gmail.com>
2026-02-17 16:31:15 +0700

    week03: simulate stack overflow error in setter

---
+++ src/oop_135150_suryanandadestiawan/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_001_suryanandadestiawan.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("---test error---")
+    e.salary = 5000 //bakal ngeledakin program
+}
--- src/oop_135150_suryanandadestiawan/week03/employee.kt
+++ src/oop_135150_suryanandadestiawan/week03/employee.kt
@@ -2,6 +2,11 @@ package oop_001_suryanandadestiawan.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            //Kode di bawah ini sangat berbahaya
+            this.salary = value
+        }
 }
 
 
