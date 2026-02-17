d91eaff37adb6cc59cf46c7c0348d1e423bab7f1
suryananndaa <suryananda7963@gmail.com>
2026-02-17 16:41:36 +0700

    week03: add private property

--- src/oop_135150_suryanandadestiawan/week03/employee.kt
+++ src/oop_135150_suryanandadestiawan/week03/employee.kt
@@ -1,15 +1,29 @@
 package oop_001_suryanandadestiawan.week03
 
 class Employee(val name: String) {
+
+    // Encapsulation untuk salary
     var salary: Int = 0
         set(value) {
             if (value < 0) {
                 println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
-                field = 0 //gunakan field, bukan this.salary
+                field = 0
             } else {
-                field = value //gunakan field untuk assign nilai
+                field = value
             }
         }
+
+    // Hanya bisa diakses di dalam class
+    private var performanceRating: Int = 3
+
+    // Function public untuk menaikkan rating
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    // Function untuk melihat status karyawan
+    fun printStatus() {
+        println("Karyawan: $name, Gaji: $salary, Rating: $performanceRating")
+    }
 }
-
-
