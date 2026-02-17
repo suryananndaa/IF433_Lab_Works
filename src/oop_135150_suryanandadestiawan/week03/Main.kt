e40f701f1365a521a5a324d06f930a85b808af3a
suryananndaa <suryananda7963@gmail.com>
2026-02-17 16:47:01 +0700

    week03: cleanup code

--- src/oop_135150_suryanandadestiawan/week03/Main.kt
+++ src/oop_135150_suryanandadestiawan/week03/Main.kt
@@ -3,12 +3,10 @@ package oop_001_suryanandadestiawan.week03
 fun main() {
     val e = Employee("Budi")
 
-    //tes validasi
-    e.salary = -1000 //harusnya print error
+
+    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
-    // test encapsulation
     e.increasePerformance()
-    //tes computed property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
--- src/oop_135150_suryanandadestiawan/week03/employee.kt
+++ src/oop_135150_suryanandadestiawan/week03/employee.kt
@@ -2,7 +2,6 @@ package oop_001_suryanandadestiawan.week03
 
 class Employee(val name: String) {
 
-    // Encapsulation untuk salary
     var salary: Int = 0
         set(value) {
             if (value < 0) {
@@ -13,16 +12,13 @@ class Employee(val name: String) {
             }
         }
 
-    // Hanya bisa diakses di dalam class
     private var performanceRating: Int = 3
 
-    // Function public untuk menaikkan rating
     fun increasePerformance() {
         performanceRating++
         println("Kinerja $name meningkat! Rating: $performanceRating")
     }
 
-    // Function untuk melihat status karyawan
     fun printStatus() {
         println("Karyawan: $name, Gaji: $salary, Rating: $performanceRating")
     }
