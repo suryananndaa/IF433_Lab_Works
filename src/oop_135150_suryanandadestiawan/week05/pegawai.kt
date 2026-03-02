10e2b4c149c854e4460f79b7399466cf4e3349b1
suryananndaa <suryananda7963@gmail.com>
2026-03-02 23:14:27 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_135150_suryanandadestiawan/week05/pegawai.kt
@@ -0,0 +1,6 @@
+package oop_001_suryanandadestiawan.week05
+
+abstract class Pegawai(val nama: String) {
+    // Abstract function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}
