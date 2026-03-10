63b89d412ce9aeb476a8529b4cd1604a8095c494
suryananndaa <suryananda7963@gmail.com>
2026-03-10 21:25:44 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_135150_suryanandadestiawan/week06/SmartHomeHub.kt
+++ src/oop_135150_suryanandadestiawan/week06/SmartHomeHub.kt
@@ -14,7 +14,19 @@ class SmartHomeHub {
         println("\n Mematikan semua perangkat Switchable...")
         for (device in devices) {
             if (device is Switchable) {
-                device.turnOff() // Smart Cast: otomatis jadi Switchable di sini
+                device.turnOff()
+            }
+        }
+    }
+
+    fun activateSecurityMode() {
+        println("\n MODE KEAMANAN DIAKTIFKAN!")
+        for (device in devices) {
+            if (device is Recordable) {
+                device.startRecord() // Smart Cast: otomatis jadi Recordable
+            }
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan") // Smart Cast: otomatis jadi SmartSpeaker
             }
         }
     }
