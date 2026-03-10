b72aadea42c20c5cba988f56ac426d8c8871aa72
suryananndaa <suryananda7963@gmail.com>
2026-03-10 20:56:45 +0700

    week06: fix Clickable interface using abstract property

--- src/oop_135150_suryanandadestiawan/week06/clickable.kt
+++ src/oop_135150_suryanandadestiawan/week06/clickable.kt
@@ -1,10 +1,6 @@
 package oop_001_suryanandadestiawan.week06
 
 interface Clickable {
-
-    // ERROR: Property in an interface cannot have a backing field
-    val name: String = "Tombol Rahasia"
-
-    // Function without body (Implicitly Abstract)
+    val name: String // Abstract property, tidak ada nilai default
     fun click()
 }
