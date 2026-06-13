d5eadc0080c9639d3101d2d8a6ca1bfdc750da1c
Surya <suryananda7963@gmail.com>
2026-06-13 08:18:43 +0700

    week11: create string extension functions

---
+++ src/oop_135150_suryanandadestiawan/week11/StringExtensions.kt
@@ -0,0 +1,11 @@
+package oop_135150_suryanandadestiawan.week11
+
+// Extension function dengan receiver 'this'
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+// Extension function dengan parameter
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
