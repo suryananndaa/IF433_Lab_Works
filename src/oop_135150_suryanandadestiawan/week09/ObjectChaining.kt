5edbf69151b701399174a40c58d05fc03beb161d
Surya <suryananda7963@gmail.com>
2026-06-13 07:43:17 +0700

    week09: create Student data class and list

---
+++ src/oop_135150_suryanandadestiawan/week09/ObjectChaining.kt
@@ -0,0 +1,12 @@
+package oop_135150_suryanandadestiawan.week09
+
+data class Student(val name: String, val gpa: Double)
+
+fun main() {
+    val students = listOf(
+        Student("Andi", 3.2),
+        Student("Budi", 3.9),
+        Student("Citra", 2.8),
+        Student("Dewi", 3.7)
+    )
+}
