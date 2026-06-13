ae51ab7f3d509351e92a7d93260dbbf2dab7dadf
Surya <suryananda7963@gmail.com>
2026-06-13 08:27:26 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_135150_suryanandadestiawan/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_135150_suryanandadestiawan.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
