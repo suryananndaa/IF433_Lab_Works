2a8ac43d686ca73eb14a8eaa5e260283bce2c16b
Surya <suryananda7963@gmail.com>
2026-06-13 07:56:19 +0700

    week09: (task) print worst performers and unique pairs

--- src/oop_135150_suryanandadestiawan/week09/TradingDashboard.kt
+++ src/oop_135150_suryanandadestiawan/week09/TradingDashboard.kt
@@ -40,4 +40,10 @@ fun main() {
     println("=== CRYPTO TRADING DASHBOARD ===")
     println("\n--- TOP PERFORMERS (WIN) ---")
     topPerformersString.forEach { println(it) }
+
+    println("\n--- WORST PERFORMERS (LOSS) ---")
+    worstPerformersString.forEach { println(it) }
+
+    println("\n--- UNIQUE TRADING PAIRS ---")
+    println("Pairs traded: $uniquePairs")
 }
