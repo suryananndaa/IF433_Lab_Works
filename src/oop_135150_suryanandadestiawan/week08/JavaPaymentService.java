eea30c2c493cf4ea19bd0286431b304ea1b91729
Surya <suryananda7963@gmail.com>
2026-06-13 07:35:39 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_135150_suryanandadestiawan/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_135150_suryanandadestiawan.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
