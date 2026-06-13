066f905cb4be3f139170b0d8bfe04b449397bba6
Surya <suryananda7963@gmail.com>
2026-06-13 09:04:11 +0700

    week13: implement toCsv and fromCsv logic

--- src/oop_135150_suryanandadestiawan/week13/StudentSerialization.kt
+++ src/oop_135150_suryanandadestiawan/week13/StudentSerialization.kt
@@ -4,3 +4,12 @@ import java.io.File
 import java.io.FileNotFoundException
 
 data class Student(val name: String, val age: Int, val gpa: Double)
+
+// Serialization (Object -> CSV)
+fun Student.toCsv(): String = "$name,$age,$gpa"
+
+// Deserialization (CSV -> Object)
+fun fromCsv(line: String): Student {
+    val parts = line.split(delimiters = ",")
+    return Student(name = parts[0], age = parts[1].toInt(), gpa = parts[2].toDouble())
+}
