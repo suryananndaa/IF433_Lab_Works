fc541c32529a43b29c9b24ba390f7d23c8836407
Surya <suryananda7963@gmail.com>
2026-06-13 09:06:09 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_135150_suryanandadestiawan/week13/CryptoTradeLogger.kt
@@ -0,0 +1,12 @@
+package oop_135150_suryanandadestiawan.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
