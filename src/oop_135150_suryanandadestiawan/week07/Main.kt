b302643106710039ac5edc59d267aae98cc57606
Surya <suryananda7963@gmail.com>
2026-06-13 07:01:36 +0700

    week07: test Singleton and Companion Object in main

--- src/oop_135150_suryanandadestiawan/week07/Main.kt
+++ src/oop_135150_suryanandadestiawan/week07/Main.kt
@@ -1,6 +1,11 @@
 package oop_135150_suryanandadestiawan.week07
 
 fun main() {
-    // CHECKPOINT 2: This line will cause a compiler error because the constructor is private
-    val client = NetworkClient("https://api.umn.ac.id")
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient() // Instansiasi lewat Factory
+    client.connect()
 }
