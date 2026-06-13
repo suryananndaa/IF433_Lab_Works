5b55aa242553efbd6ce5654a9ba9e68cbba13826
Surya <suryananda7963@gmail.com>
2026-06-13 08:12:13 +0700

    week10: (task) populate coin repository

--- src/oop_135150_suryanandadestiawan/week10/CryptoDashboard.kt
+++ src/oop_135150_suryanandadestiawan/week10/CryptoDashboard.kt
@@ -2,4 +2,9 @@ package oop_135150_suryanandadestiawan.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    // Mengisi dompet dengan koin
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 3.2))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
