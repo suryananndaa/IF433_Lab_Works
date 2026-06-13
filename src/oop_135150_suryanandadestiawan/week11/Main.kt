215a6f16d1b96e8e4789db053a281ed465488623
Surya <suryananda7963@gmail.com>
2026-06-13 08:20:03 +0700

    week11: test extension functions in main

---
+++ src/oop_135150_suryanandadestiawan/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_135150_suryanandadestiawan.week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
