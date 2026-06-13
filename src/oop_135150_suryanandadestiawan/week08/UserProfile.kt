e813642bdd1078aaf7e5132957f3c61c069cf11b
Surya <suryananda7963@gmail.com>
2026-06-13 07:15:10 +0700

    week08: create basic UserProfile with nullable fields

---
+++ src/oop_135150_suryanandadestiawan/week08/UserProfile.kt
@@ -0,0 +1,8 @@
+package oop_135150_suryanandadestiawan.week08
+
+// name adalah Non-Null (wajib), email dan phone adalah Nullable (opsional)
+class UserProfile(
+    val name: String,
+    val email: String?,
+    val phone: String? = null // Default argument null
+)
