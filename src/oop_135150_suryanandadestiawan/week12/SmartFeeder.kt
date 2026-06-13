13e11687bced7f7f9667af02b7285eef6b82d229
Surya <suryananda7963@gmail.com>
2026-06-13 08:51:02 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_135150_suryanandadestiawan/week12/SmartFeeder.kt
@@ -0,0 +1,6 @@
+package oop_135150_suryanandadestiawan.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+    return availableGram - requestedGram
+}
