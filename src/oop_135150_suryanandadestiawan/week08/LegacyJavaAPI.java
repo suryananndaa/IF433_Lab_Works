47a4b5fc393c7cf0f700dca38caf1aeb4e027b52
Surya <suryananda7963@gmail.com>
2026-06-13 07:31:05 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_135150_suryanandadestiawan/week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_135150_suryanandadestiawan.week08;
+
+public class LegacyJavaAPI {
+    // Fungsi ini ditulis dalam Java. Kotlin tidak tahu apakah kembaliannya Nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
