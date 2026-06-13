e60046c394612c8f44620402f86e9d148129676a
Surya <suryananda7963@gmail.com>
2026-06-13 07:06:39 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_135150_suryanandadestiawan/week07/GameManager.kt
@@ -0,0 +1,14 @@
+package oop_135150_suryanandadestiawan.week07
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
