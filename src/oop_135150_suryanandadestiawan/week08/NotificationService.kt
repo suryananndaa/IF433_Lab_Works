116a982ef0feb936baf78367f530b11fef48891f
Surya <suryananda7963@gmail.com>
2026-06-13 07:16:06 +0700

    week08: implement smart cast in function

---
+++ src/oop_135150_suryanandadestiawan/week08/NotificationService.kt
@@ -0,0 +1,18 @@
+package oop_135150_suryanandadestiawan.week08
+
+class NotificationService {
+    fun sendEmail(emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile) {
+        // sendEmail(user.email) // INI AKAN ERROR: Type mismatch
+
+        if (user.email != null) {
+            // SUCCESS via Smart Cast: compiler tahu 'user.email' pasti tidak null di blok ini
+            sendEmail(emailAddress = user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
+}
