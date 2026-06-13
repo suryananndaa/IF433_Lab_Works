e5be8abacec38af6c0ed50304da18e1c1d17c227
Surya <suryananda7963@gmail.com>
2026-06-13 07:08:42 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/oop_135150_suryanandadestiawan/week07/BattleEvent.kt
@@ -0,0 +1,8 @@
+package oop_135150_suryanandadestiawan.week07
+
+sealed class BattleState {
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+    data class LootDropped(val item: GameItem) : BattleState()
+    data class GameOver(val reason: String) : BattleState()
+    object SafeZone : BattleState()
+}
